2fbff20a0ca4b97c76927ca066293fec8a877196
cvmartin <cvarelamail@gmail.com>
2018-10-17 16:11:21 +0200

    dissagregate utils file

---
+++ R/time.R
@@ -0,0 +1,31 @@
+
+#' Convert a data frame in a time series
+#'
+#' @param df
+#'
+#' @return time series
+#' @import zoo
+#' @import xts
+#' @import dygraphs
+#' @import lubridate
+#' @import shiny
+#' @import shinyWidgets
+#' @export
+#'
+#' @examples
+#' 1+1
+df_to_ts <- function(df) {
+  require(xts)
+  new_ts <- xts(x = df[, 2:(length(colnames(df)))], order.by = df[[1]])
+  if (is.null(colnames(new_ts))) colnames(new_ts) <- colnames(df[2])
+  return(new_ts)
+}
+
+ts_to_df <- function(tseries){
+  require(xts)
+  new_df <- as_tibble(data.frame(datetime=index(tseries), coredata(tseries)))
+}
+
+
+# validation --------------------------------------------------------------
+is_POSIXt <- function(x) inherits(x, "POSIXt")
